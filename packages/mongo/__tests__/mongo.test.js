72d533a000447b8e2d0d97421b5eecf481e77a9b
Alexandre Pereira <alexandre.pereira@foundersfactory.co>
2020-07-31 11:28:25 +0200

    📦 mongo update method and flatten util

--- packages/mongo/__tests__/mongo.test.js
+++ packages/mongo/__tests__/mongo.test.js
@@ -44,21 +44,127 @@ test('MongoPlugin should connect', async () => {
   }
 })
 
-test('MongoPlugin create is defined', async () => {
-  const s = new MongoPlugin({
-    url: MONGO_URL,
-    database: MONGO_DATABASE
+describe('Mongo Plugin - create', () => {
+  test('MongoPlugin create is defined', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    expect(s.create).toBeDefined()
+    expect(typeof s.create).toBe('function')
   })
-  expect(s.create).toBeDefined()
-  expect(typeof s.create).toBe('function')
+
+  test('MongoPlugin create should create', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    const data = { foo: 'bar' }
+
+    const result = await s.create('foo', data)
+    expect(result).toBeDefined()
+    expect(result.id).toBeDefined()
+    expect(result.data).toBe(data)
+  })
+
+  test('MongoPlugin create should throw an error if no collection specified', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    try {
+      expect(await s.create(null, { foo: 'bar' })).toThrow()
+    } catch (error) {
+      expect(error.message).toBe(MongoErrors.MissingCollectionOrData)
+    }
+  })
+
+  test('MongoPlugin create should throw an error if no data specified', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    try {
+      expect(await s.create('foo')).toThrow()
+    } catch (error) {
+      expect(error.message).toBe(MongoErrors.MissingCollectionOrData)
+    }
+  })
 })
 
-test('MongoPlugin create should create', async () => {
-  const s = new MongoPlugin({
-    url: MONGO_URL,
-    database: MONGO_DATABASE
+describe('Mongo Plugin - update', () => {
+  test('MongoPlugin update should update', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    const data = { foo: 'bar' }
+    const updateData = { foo: 'bar-update' }
+
+    const inserted = await s.create('foo', data)
+    expect(inserted).toBeDefined()
+    const updated = await s.update('foo', updateData, inserted.id)
+    expect(updated).toBeDefined()
+    expect(updated.data).toBeDefined()
+    expect(updated.data.foo).toBe(updateData.foo)
   })
-  const inserted = await s.create('foo', { foo: 'bar ' })
-  expect(inserted).toBeDefined()
-  expect(inserted.insertedCount).toBe(1)
+
+  test('MongoPlugin update should update (id from data._id)', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    const data = { foo: 'bar' }
+
+    const inserted = await s.create('foo', data)
+    expect(inserted).toBeDefined()
+    const updateData = { foo: 'bar-update' } 
+    const updated = await s.update('foo', Object.assign(inserted.data, updateData))
+    expect(updated).toBeDefined()
+    expect(updated.data).toBeDefined()
+    expect(updated.data.foo).toBe(updateData.foo)
+  })
+
+  test('MongoPlugin update should throw an error if no collection specified', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    try {
+      const data = { foo: 'bar' }
+      const inserted = await s.create('foo', data)
+      expect(inserted).toBeDefined()
+      expect(await s.update(null, { foo: 'bar-update' }, inserted.id)).toThrow()
+    } catch (error) {
+      expect(error.message).toBe(MongoErrors.MissingCollectionOrData)
+    }
+  })
+
+  test('MongoPlugin update should throw an error if no data specified', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    try {
+      const data = { foo: 'bar' }
+      const inserted = await s.create('foo', data)
+      expect(inserted).toBeDefined()
+      expect(await s.update('foo', null, inserted.id)).toThrow()
+    } catch (error) {
+      expect(error.message).toBe(MongoErrors.MissingCollectionOrData)
+    }
+  })
+
+  test('MongoPlugin update should throw an error if no id specified', async () => {
+    const s = new MongoPlugin({
+      url: MONGO_URL,
+      database: MONGO_DATABASE
+    })
+    try {
+      const data = { foo: 'bar' }
+      expect(await s.update('foo', data)).toThrow()
+    } catch (error) {
+      expect(error.message).toBe(MongoErrors.MissingId)
+    }
+  })
 })
--- packages/mongo/src/index.ts
+++ packages/mongo/src/index.ts
@@ -1,12 +1,21 @@
 import { MongoClient } from 'mongodb'
 
+import { flatten } from './utils'
+
 interface MongoOptions {
   url: string,
   database: string
 }
 
+interface MongoResult {
+  id: string,
+  data: Record<string, any>
+}
+
 export enum MongoErrors {
   MissingOptions = 'Missing options at mongo instanciation, `database & url` are required',
+  MissingCollectionOrData = 'Missing collection or data in query',
+  MissingId = 'Missing `id` or `_id` in params or data'
 }
 
 export class MongoPlugin {
@@ -27,7 +36,7 @@ export class MongoPlugin {
     this.mongoUrl = options.url
   }
 
-  async connect () {
+  async connect (): Promise<Object> {
     const client = await MongoClient.connect(this.mongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -39,10 +48,36 @@ export class MongoPlugin {
     return { client: this.client, db: this.db }
   }
 
-  async create (collection: string, data: Object) {
+  async create (collection: string, data: Object): Promise<MongoResult> {
+    if (!collection || !data) throw new Error(MongoErrors.MissingCollectionOrData)
+
     await this.connect()
     const inserted = await this.db.collection(collection).insertOne(data)
     this.client.close()
-    return inserted
+
+    const { ops, insertedId } = inserted
+    return { id: insertedId, data: ops[0] }
+  }
+
+  async update (collection: string, data: Record<string, any>, id: string) : Promise<MongoResult> {
+    if (!collection || !data) throw new Error(MongoErrors.MissingCollectionOrData)
+    if (!id && !data._id) throw new Error(MongoErrors.MissingId)
+
+    await this.connect()
+
+    const { _id, ...rest } = data
+    const flatData = flatten(rest)
+
+    const updated = await this.db.collection(collection).findOneAndUpdate({
+      _id: id || _id
+    }, {
+      $set: flatData
+    }, {
+      returnOriginal: false
+    })
+    this.client.close()
+
+    const { value } = updated
+    return { id: value._id, data: value }
   }
 }
