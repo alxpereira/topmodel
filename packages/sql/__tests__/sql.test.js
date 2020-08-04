4a36d20ef1f6bc7bbfb0558608e9ce42af52fed9
Alexandre Pereira <alexandre.pereira@foundersfactory.co>
2020-08-04 17:19:14 +0200

    🏗  SQL crud

--- packages/sql/__tests__/sql.test.js
+++ packages/sql/__tests__/sql.test.js
@@ -2,21 +2,21 @@
 import { SQLPlugin, SQLPluginErrors } from '../src'
 import mockKnex from 'mock-knex'
 
-const PSQL_HOST = process.env.PSQL_HOST
+const PSQL_HOST = '127.0.0.1'
 const PSQL_USER = 'jest'
 const PSQL_PASSWORD = 'jest'
-const PSQL_DATABASE = 'jest'
+const PSQL_DATABASE = 'foo'
+
+const s = new SQLPlugin({
+  client: 'pg',
+  host: PSQL_HOST,
+  user: PSQL_USER,
+  password: PSQL_PASSWORD,
+  database: PSQL_DATABASE
+})
 
 describe('SQL Plugin Setup', () => {
   test('MongoPlugin is a Class', () => {
-    const s = new SQLPlugin({
-      client: 'pg',
-      host: PSQL_HOST,
-      user: PSQL_USER,
-      password: PSQL_PASSWORD,
-      database: PSQL_DATABASE
-    })
-
     expect(s).toBeInstanceOf(SQLPlugin)
   })
 
@@ -31,15 +31,13 @@ describe('SQL Plugin Setup', () => {
 })
 
 describe('SQL Plugin - Create', () => {
+  beforeEach(() => {
+    mockKnex.mock(s.knex)
+  })
+  afterEach(() => {
+    mockKnex.unmock(s.knex)
+  })
   test('SQLPlugin.create should throw if no table is specified', async () => {
-    const s = new SQLPlugin({
-      client: 'pg',
-      host: PSQL_HOST,
-      user: PSQL_USER,
-      password: PSQL_PASSWORD,
-      database: PSQL_DATABASE
-    })
-
     try {
       expect(await s.create(null, {})).toThrow()
     } catch (error) {
@@ -49,14 +47,6 @@ describe('SQL Plugin - Create', () => {
   })
 
   test('SQLPlugin.create should throw if no data is specified', async () => {
-    const s = new SQLPlugin({
-      client: 'pg',
-      host: PSQL_HOST,
-      user: PSQL_USER,
-      password: PSQL_PASSWORD,
-      database: PSQL_DATABASE
-    })
-
     try {
       expect(await s.create('foo')).toThrow()
     } catch (error) {
@@ -65,17 +55,148 @@ describe('SQL Plugin - Create', () => {
     }
   })
 
-  test('SQLPlugin.create should throw if no data is specified', async () => {
-    const s = new SQLPlugin({
-      client: 'pg',
-      host: PSQL_HOST,
-      user: PSQL_USER,
-      password: PSQL_PASSWORD,
-      database: PSQL_DATABASE
+  test('SQLPlugin.create should create', async () => {
+    const tracker = mockKnex.getTracker()
+    tracker.install()
+    tracker.on('query', query => {
+      query.response([{ foo: 'bar' }])
     })
 
-    mockKnex.mock(s.knex)
+    const data = { foo: 'bar' }
+    const result = await s.create('foo', data)
+    expect(result).toBeDefined()
+    expect(result.data.foo).toBe(data.foo)
+    tracker.uninstall()
+  })
+})
 
-    await s.create('foo', { foo: 'bar' })
+describe('SQL Plugin - Read', () => {
+  beforeEach(() => {
+    mockKnex.mock(s.knex)
+  })
+  afterEach(() => {
+    mockKnex.unmock(s.knex)
+  })
+  test('SQLPlugin.read should throw if no table is specified', async () => {
+    try {
+      expect(await s.read(null, {})).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingTable)
+    }
+  })
+
+  test('SQLPlugin.read should throw if no where condition is specified', async () => {
+    try {
+      expect(await s.read('foo')).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingSelectorWhere)
+    }
+  })
+
+  test('SQLPlugin.read should read', async () => {
+    const tracker = mockKnex.getTracker()
+    tracker.install()
+    tracker.on('query', query => {
+      query.response([{ foo: 'bar' }])
+    })
+
+    const data = { foo: 'bar' }
+    const result = await s.read('foo', data)
+    expect(result).toBeDefined()
+    expect(result.data.foo).toBe(data.foo)
+    tracker.uninstall()
+  })
+})
+
+describe('SQL Plugin - Update', () => {
+  beforeEach(() => {
+    mockKnex.mock(s.knex)
+  })
+  afterEach(() => {
+    mockKnex.unmock(s.knex)
+  })
+  test('SQLPlugin.update should throw if no table is specified', async () => {
+    try {
+      expect(await s.update(null, {})).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingTable)
+    }
+  })
+
+  test('SQLPlugin.update should throw if no where condition is specified', async () => {
+    try {
+      expect(await s.update('foo')).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingSelectorWhere)
+    }
+  })
+
+  test('SQLPlugin.update should throw if no data is specified', async () => {
+    try {
+      expect(await s.update('foo', { foo: 'bar' })).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingData)
+    }
+  })
+
+  test('SQLPlugin.update should update', async () => {
+    const tracker = mockKnex.getTracker()
+    const data = { foo: 'bar-update' }
+
+    tracker.install()
+    tracker.on('query', query => {
+      query.response([data])
+    })
+
+    const result = await s.update('foo', { foo: 'bar' }, data)
+    expect(result).toBeDefined()
+    expect(result.data.foo).toBe(data.foo)
+    tracker.uninstall()
+  })
+})
+
+describe('SQL Plugin - Delete', () => {
+  beforeEach(() => {
+    mockKnex.mock(s.knex)
+  })
+  afterEach(() => {
+    mockKnex.unmock(s.knex)
+  })
+  test('SQLPlugin.del should throw if no table is specified', async () => {
+    try {
+      expect(await s.del(null, {})).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingTable)
+    }
+  })
+
+  test('SQLPlugin.del should throw if no where condition is specified', async () => {
+    try {
+      expect(await s.del('foo')).toThrow()
+    } catch (error) {
+      expect(error).toBeDefined()
+      expect(error.message).toBe(SQLPluginErrors.MissingSelectorWhere)
+    }
+  })
+
+  test('SQLPlugin.del should delete', async () => {
+    const tracker = mockKnex.getTracker()
+    const data = { foo: 'bar-update' }
+
+    tracker.install()
+    tracker.on('query', query => {
+      query.response([data])
+    })
+
+    const result = await s.del('foo', { foo: 'bar' })
+    expect(result).toBeDefined()
+    expect(result.success).toBe(true)
+    tracker.uninstall()
   })
 })
--- packages/sql/src/index.ts
+++ packages/sql/src/index.ts
@@ -12,14 +12,16 @@ interface SQLOptions {
 }
 
 interface SQLResult {
-    id: string,
-    data: Record<string, any>
+  id?: string
+  data?: Record<string, any>,
+  success?: boolean
 }
 
 export enum SQLPluginErrors {
   MissingOptions = 'Missing some options in Plugin instanciation',
   MissingTable = 'Missing table to target',
-  MissingData = 'Missing data in query'
+  MissingData = 'Missing data in query',
+  MissingSelectorWhere = 'Missing where condition to target your object'
 }
 
 export class SQLPlugin {
@@ -41,7 +43,42 @@ export class SQLPlugin {
     if (!table) throw new Error(SQLPluginErrors.MissingTable)
     if (!data) throw new Error(SQLPluginErrors.MissingData)
 
-    const results = await this.knex(table).insert(data).returning('*')
-    return results
+    const [result] = await this.knex(table).insert(data).returning('*')
+    return { data: result }
+  }
+
+  async read (table: string, where: Record<string, any>): Promise<SQLResult> {
+    if (!table) throw new Error(SQLPluginErrors.MissingTable)
+    if (!where) throw new Error(SQLPluginErrors.MissingSelectorWhere)
+
+    const [result] = await this.knex(table).where(where).returning('*')
+
+    return { data: result }
+  }
+
+  async update (
+    table: string,
+    where: Record<string, any>,
+    data: Record<string, any>
+  ): Promise<SQLResult> {
+    if (!table) throw new Error(SQLPluginErrors.MissingTable)
+    if (!where) throw new Error(SQLPluginErrors.MissingSelectorWhere)
+    if (!data) throw new Error(SQLPluginErrors.MissingData)
+
+    const [result] = await this.knex(table)
+      .where(where)
+      .update(data)
+      .returning('*')
+
+    return { data: result }
+  }
+
+  async del (table: string, where: Record<string, any>): Promise<SQLResult> {
+    if (!table) throw new Error(SQLPluginErrors.MissingTable)
+    if (!where) throw new Error(SQLPluginErrors.MissingSelectorWhere)
+
+    await this.knex(table).where(where).del()
+
+    return { success: true }
   }
 }
